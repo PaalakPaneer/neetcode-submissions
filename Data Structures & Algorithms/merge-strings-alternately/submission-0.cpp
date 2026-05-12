7b0ca7573f556e97f2796d8bb5fa1131e37a6909
PaalakPaneer <76420677+PaalakPaneer@users.noreply.github.com>
2026-05-13 00:42:12 +0530

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int i=0,j=0;
+        string word3 = "";
+        while(i != word1.size() || j != word2.size()){
+            if(i!= word1.size()){
+                word3 += word1[i];
+                i++;
+            }
+            if(j!= word2.size()){
+                word3 += word2[j];
+                j++;
+            }
+        }
+        return word3;        
+    }
+};
