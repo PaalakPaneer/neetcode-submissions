9f240288c2d6757761e0df4636d9ed75739f1514
PaalakPaneer <76420677+PaalakPaneer@users.noreply.github.com>
2026-05-28 23:33:46 +0530

    Add: boats-to-save-people - submission-1

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int l = 0;
+        int r = people.size()-1;
+        int ans = 0;
+        sort(people.begin(), people.end());
+        while(l<=r){
+            int sum  = people[l] + people[r];
+            if(people[l] + people[r] <=limit){
+                l++;
+            }
+            r--;
+            ans++;
+        }
+        return ans;
+    }
+};
